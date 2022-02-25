49e1e205923d2ab92eb12aa52b899a5d4189f63e
retchohrips <44993244+retchohrips@users.noreply.github.com>
2022-02-25 15:39:25 -0500

    Improved a11y
    - phone menu tabbable, with button role, and triggered by enter or space
    - header properly tagged

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 
-navBarToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('active');
+navBarToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
+});
+
+navBarToggle.addEventListener("keydown", (event) => {
+  if (event.code === "Space" || event.code === "Enter") {
+    mainNav.classList.toggle("active");
+  }
 });
