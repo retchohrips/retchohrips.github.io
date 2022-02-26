803aa562945ed04c042d2831547cb43c26fe3087
retchohrips <44993244+retchohrips@users.noreply.github.com>
2022-02-26 18:50:38 -0500

    Add toad page and simplify class names
    - Add toad.html
    - Add hero classes
    - Simplify class names
    - All fonts changed

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
+let navToggle = document.getElementById("js-nav-toggle");
 
-navBarToggle.addEventListener("click", function () {
+navToggle.addEventListener("click", function () {
   mainNav.classList.toggle("active");
 });
 
-navBarToggle.addEventListener("keydown", (event) => {
+navToggle.addEventListener("keydown", (event) => {
   if (event.code === "Space" || event.code === "Enter") {
     mainNav.classList.toggle("active");
   }
